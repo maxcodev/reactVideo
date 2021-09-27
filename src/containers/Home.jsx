453684a702430a5c269747ee48730156d0a7ea1c
Máximo Sánchez <id569723@outlook.com>
2021-09-27 10:36:48 -0500

    dependencie added (react-router-dom), file and function modified -> Home. Route name: (App) and index.js modified, i imported route's folder and used the App's file

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -12,7 +12,7 @@ import '../assets/styles/App.scss';
 
 const API = 'http://localhost:3000/initalState';
 
-const App = () => {
+const Home = () => {
   const initialState = useInitialState(API);
   return (
     <div className='App'>
@@ -56,4 +56,4 @@ const App = () => {
     </div>
   );
 };
-export default App;
+export default Home;
